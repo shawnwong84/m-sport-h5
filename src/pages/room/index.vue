1012181d7be1999a5b14f239e25fd100745f0929
full <zj15355208214@gmail.com>
2022-01-03 19:47:03 +0800

    复制

--- src/pages/room/index.vue
+++ src/pages/room/index.vue
@@ -1026,31 +1026,31 @@ export default {
             this.$toast({
                 message: 'QQ号：' + text + '，已复制成功',
             });
-            window.open(
-                'tencent://message/?Menu=yes&uin=' +
-                    text +
-                    '&Site=80fans&Service=300&sigT=45a1e5847943b64c6ff3990f8a9e644d2b31356cb0b4ac6b24663a3c8dd0f8aa12a545b1714f9d45',
-                '_blank',
-            );
+            // window.open(
+            //     'tencent://message/?Menu=yes&uin=' +
+            //         text +
+            //         '&Site=80fans&Service=300&sigT=45a1e5847943b64c6ff3990f8a9e644d2b31356cb0b4ac6b24663a3c8dd0f8aa12a545b1714f9d45',
+            //     '_blank',
+            // );
         },
         openWx(text) {
             this.cotyTxt(text);
             this.$toast({
                 message: '微信号：' + text + '，已复制成功',
             });
-            window.open('weixin://');
+            // window.open('weixin://');
         },
         openqqGroup(text) {
             this.cotyTxt(text);
             this.$toast({
                 message: 'QQ群号：' + text + '，已复制成功',
             });
-            window.open(
-                'tencent://message/?Menu=yes&uin=' +
-                    text +
-                    '&Site=80fans&Service=300&sigT=45a1e5847943b64c6ff3990f8a9e644d2b31356cb0b4ac6b24663a3c8dd0f8aa12a545b1714f9d45',
-                '_blank',
-            );
+            // window.open(
+            //     'tencent://message/?Menu=yes&uin=' +
+            //         text +
+            //         '&Site=80fans&Service=300&sigT=45a1e5847943b64c6ff3990f8a9e644d2b31356cb0b4ac6b24663a3c8dd0f8aa12a545b1714f9d45',
+            //     '_blank',
+            // );
         },
     },
 };
